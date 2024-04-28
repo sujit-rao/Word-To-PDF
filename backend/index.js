5a0d0114dfcebd5b5955166e07dbe86bd6a15719
Sujit Rao <sujitrao867@gmail.com>
2024-04-28 21:35:11 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,56 @@
+import  multer  from 'multer'
+import express from 'express'
+import docxConverter from 'docx-pdf'
+import path from 'path'
+import cors from 'cors'
+
+
+const app = express()
+const port = 3000
+
+app.use(cors())
+
+//storage
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads')
+    },
+    filename: function (req, file, cb) {
+
+        cb(null, file.originalname)
+    }
+})
+
+const upload = multer({ storage: storage })
+
+app.post('/convertFile', upload.single('file'), (req, res, next) => {
+    try {
+
+        if (!req.file) {
+            return res.status(400).json({
+                message: "No file uploaded",
+            })
+        }
+        //output file path
+        let outputPath = path.join(
+              "files", `${req.file.originalname}.pdf`);
+
+        docxConverter(req.file.path, outputPath, (err, result) => {
+            if (err) {
+                console.log(err);
+                return res.status(500).json({message:"Error converting docx to pdf ", })
+            }
+            res.download(outputPath,()=>{
+                console.log("file download")
+            })
+            console.log('result' + result);
+        });
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message:"Internal server error ", })
+    }
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export default function Navbar() {
+    return (
+        <>
+            <div className=' max-w-screen-2xl mx-auto container px-6 md:px-40 shadow-lg h-16 font-bold pt-4 fixed'>
+                <div className=' flex justify-between'>
+                    <h1 className=' text-3xl cursor-pointer'>Word<span className=' text-blue-500 text-xl'>TO</span>pdf</h1>
+                    <h1 className=' text-2xl cursor-pointer hover:scale-110 duration-200 '>Home</h1>
+                </div>
+            </div>
+        </>
+
+    )
+}
